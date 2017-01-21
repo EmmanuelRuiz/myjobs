859a99560b1830f2b7739b36b2adf083c7db291c
Emmanuel Ruiz <emmanuelre123@outlook.com>
2017-01-21 01:25:59 -0600

    SQL de la base de datos y diagrama

---
+++ bd.sql
@@ -0,0 +1,103 @@
+CREATE DATABASE IF NOT EXISTS myjobsadvisor;
+USE myjobsadvisor;
+
+CREATE TABLE users(
+id                  int(255) auto_increment not null,
+name                varchar(255) not null,
+plastname           varchar(255) not null,
+mlastname           varchar(255) not null,
+age					int(3),
+email               varchar(255),
+telephone			varchar(20),
+password            varchar(255) not null,
+connection			datetime,
+biography			text,
+image               varchar(255),
+termscondition      varchar(50),
+datejob				datetime,
+privacy				varchar(50),
+role                varchar(20) not null,
+created_at          datetime,
+updated_at			datetime,
+CONSTRAINT pk_users PRIMARY KEY(id)
+)ENGINE=InnoDb;
+
+
+CREATE TABLE companies(
+id         		int(255) auto_increment not null,
+user_id    		int(255) not null,
+tradename  		varchar(255),
+businessname	varchar(255),
+rfc				varchar(50),
+logo	        varchar(255),
+description 	text,
+businesssector	varchar(255),
+contacname 		varchar(250),
+position		varchar(250),
+telephoneext	varchar(250),
+businessemail 	varchar(250),
+status    		varchar(20),
+created_at  	datetime DEFAULT NULL,
+updated_at 		datetime DEFAULT NULL,
+CONSTRAINT  pk_companies PRIMARY KEY(id),
+CONSTRAINT  fk_companies_users FOREIGN KEY(user_id) REFERENCES users(id)
+)ENGINE=InnoDb;
+
+CREATE TABLE opinions(
+id          	int(255) auto_increment not null,
+company_id    	int(255) not null,
+user_id     	int(255) not null,
+generalcomment  text,
+point1			varchar(250),
+point2			varchar(250),
+point3			varchar(250),
+point4			varchar(250),
+point5			varchar(250),
+point6			varchar(250),
+point7			varchar(250),
+point8			varchar(250),
+point9			varchar(250),
+point10			varchar(250),
+created_at  	datetime DEFAULT NULL,
+updated_at		datetime DEFAULT NULL,
+CONSTRAINT pk_opinion PRIMARY KEY(id),
+CONSTRAINT fk_opinion_company FOREIGN KEY(company_id) REFERENCES companies(id),
+CONSTRAINT fk_opinion_user  FOREIGN KEY(user_id) REFERENCES users(id)
+)ENGINE=InnoDb;
+
+CREATE TABLE comments(
+id 				int(255) not null auto_increment,
+opinion_id		int(255) not null,
+user_id 		int(255) not null,
+body			text,
+image  			varchar(255),
+created_at      datetime,
+updated_at 		datetime,
+CONSTRAINT pk_comment PRIMARY KEY(id),
+CONSTRAINT fk_comment_opinion FOREIGN KEY(opinion_id) REFERENCES opinions(id),
+CONSTRAINT fk_comment_user FOREIGN KEY(user_id) REFERENCES users(id)
+)ENGINE=InnoDb;
+
+CREATE TABLE points(
+id 				int(255) not null auto_increment,
+company_id		int(255) not null,
+user_id 		int(255) not null,
+valu	   		varchar(255),
+created_at      datetime,
+updated_at 		datetime,
+CONSTRAINT pk_point PRIMARY KEY(id),
+CONSTRAINT fk_point_company FOREIGN KEY(company_id) REFERENCES companies(id),
+CONSTRAINT fk_point_user FOREIGN KEY(user_id) REFERENCES users(id)
+)ENGINE=InnoDb;
+
+CREATE TABLE images(
+id 				int(255) not null auto_increment,
+company_id		int(255) not null,
+user_id 		int(255) not null,
+image	   		varchar(255),
+created_at      datetime,
+updated_at 		datetime,
+CONSTRAINT pk_images PRIMARY KEY(id),
+CONSTRAINT fk_image_company FOREIGN KEY(company_id) REFERENCES companies(id),
+CONSTRAINT fk_image_user FOREIGN KEY(user_id) REFERENCES users(id)
+)ENGINE=InnoDb;
