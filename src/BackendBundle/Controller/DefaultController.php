6a9ec80bc1b0d594a64148e8c8dc2a7e75a38da1
Emmanuel Ruiz <emmanuelre123@outlook.com>
2017-01-24 20:27:42 -0600

    Prueba de entidades
    Funciona correctamente

--- src/BackendBundle/Controller/DefaultController.php
+++ src/BackendBundle/Controller/DefaultController.php
@@ -8,6 +8,16 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
+		//cargar entiti manager.. acceder a nuestras entidades
+		$em = $this->getDoctrine()->getManager();
+		$user_repo = $em->getRepository("BackendBundle:User");
+		$user = $user_repo->find(1);
+		
+		echo "Bienvenido ".$user->getName()." ".$user->getplastname();
+		
+		var_dump($user);
+		die();
+		
         return $this->render('BackendBundle:Default:index.html.twig');
     }
 }
