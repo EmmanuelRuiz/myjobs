e6f17596c60ae8af34de3bae9a543817d4ce7dcd
Juan Amaya <amayajuan95@gmail.com>
2017-02-20 18:36:59 -0500

    Modificacion de metodos a su Controller
    Se modificaron los metodos a su respectivo controlador

--- src/AppBundle/Controller/CompanyController.php
+++ src/AppBundle/Controller/CompanyController.php
@@ -5,10 +5,56 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Session;
+
+use BackendBundle\Entity\Company;
+use BackendBundle\Entity\User;
 
 class CompanyController extends Controller{
 
-    public function indexAction(Request $request){
-        return $this->render('AppBundle:Company:home.html.twig');
-    }
+	// metodo para buscar empresas. DEBE ESTAR EN OTRO CONTROLADOR
+	public function companiesAction(Request $request){
+		$em = $this->getDoctrine()->getManager();
+		
+		// Hacemos una consulta a la entidad Company para que nos saque los objetos de tipo Company
+		$dql = "SELECT u FROM BackendBundle:Company u ORDER BY u.id ASC";
+		$query = $em->createQuery($dql);
+		
+		$paginator = $this->get('knp_paginator');
+		$pagination = $paginator->paginate(
+				$query, $request->query->getInt('page', 1), 5
+		);
+		
+		return $this->render('AppBundle:Companies:companies.html.twig', array(
+			'pagination' => $pagination
+		));
+	}
+	
+	// metodo para buscar empresas. DEBE ESTAR EN OTRO CONTROLADOR
+	public function searchAction(Request $request){
+		$em = $this->getDoctrine()->getManager();
+		
+		$search	= $request->query->get("search", null);
+		
+		if ($search == null) {
+			return $this->redirect($this->generateURL('home_publication'));
+		}
+		
+		// Hacemos una consulta a la entidad Company para que nos saque los objetos de tipo Company
+		// La consulta nos devuelve resultados parecidos a la busqueda que se envia
+		$dql = "SELECT u FROM BackendBundle:Company u "
+				. "WHERE u.businessname LIKE :search OR u.tradename LIKE :search "
+				. "OR u.businesssector LIKE :search ORDER BY u.id ASC";
+		$query = $em->createQuery($dql)->setParameter('search', "%$search%");
+		
+		$paginator = $this->get('knp_paginator');
+		$pagination = $paginator->paginate(
+				$query, $request->query->getInt('page', 1), 5
+		);
+		
+		return $this->render('AppBundle:Companies:companies.html.twig', array(
+			'pagination' => $pagination
+		));
+	}
 }
--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -168,8 +168,6 @@ class UserController extends Controller {
 						$user->setImage($user_image);
 					}
 					
-                
-					
                     /* volcar el objeto y persistir en doctrine */
                     $em->persist($user);
                     /* pasar los objetos persistidos a la bd */
@@ -197,50 +195,5 @@ class UserController extends Controller {
 		));
 		
     }
-	
-	// metodo para buscar empresas. DEBE ESTAR EN OTRO CONTROLADOR
-	public function companiesAction(Request $request){
-		$em = $this->getDoctrine()->getManager();
-		
-		// Hacemos una consulta a la entidad Company para que nos saque los objetos de tipo Company
-		$dql = "SELECT u FROM BackendBundle:Company u ORDER BY u.id ASC";
-		$query = $em->createQuery($dql);
-		
-		$paginator = $this->get('knp_paginator');
-		$pagination = $paginator->paginate(
-				$query, $request->query->getInt('page', 1), 5
-		);
-		
-		return $this->render('AppBundle:Companies:companies.html.twig', array(
-			'pagination' => $pagination
-		));
-	}
-	
-	// metodo para buscar empresas. DEBE ESTAR EN OTRO CONTROLADOR
-	public function searchAction(Request $request){
-		$em = $this->getDoctrine()->getManager();
-		
-		$search	= $request->query->get("search", null);
-		
-		if ($search == null) {
-			return $this->redirect($this->generateURL('home_publication'));
-		}
-		
-		// Hacemos una consulta a la entidad Company para que nos saque los objetos de tipo Company
-		// La consulta nos devuelve resultados parecidos a la busqueda que se envia
-		$dql = "SELECT u FROM BackendBundle:Company u "
-				. "WHERE u.businessname LIKE :search OR u.tradename LIKE :search "
-				. "OR u.businesssector LIKE :search ORDER BY u.id ASC";
-		$query = $em->createQuery($dql)->setParameter('search', "%$search%");
-		
-		$paginator = $this->get('knp_paginator');
-		$pagination = $paginator->paginate(
-				$query, $request->query->getInt('page', 1), 5
-		);
-		
-		return $this->render('AppBundle:Companies:companies.html.twig', array(
-			'pagination' => $pagination
-		));
-	}
 
 }
