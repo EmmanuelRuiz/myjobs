32f100560ab62c0ea6a54ab26371d48db037c052
Juan Amaya <amayajuan95@gmail.com>
2017-02-20 18:07:13 -0500

    Scroll con Ajax
    Se programo el Scroll con Ajax infinito

---
+++ web/assets/js/custom/companies.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+	
+	//alert('entro');
+
+	// Instanciamos el objeto IAS
+	var ias = jQuery.ias({
+		container: '.box-users',
+		item: '.user-item',
+		pagination: '.pagination',
+		next: '.pagination .next_link',
+		triggerPageThreshold: 5
+	});
+	
+	// Boton para ir cargando cada pagina cuando lleguemos a un limite
+	ias.extension(new IASTriggerExtension({
+		text: 'Ver mas',
+		offset: 3
+	}));
+	
+	// GIF de efecto cargando
+	ias.extension(new IASSpinnerExtension({
+		src: URL+'/../assets/images/ajax-loader.gif'
+	}));
+	
+	// Mensaje para cuando no existan mas resultados
+	ias.extension(new IASNoneLeftExtension({
+		text: 'No hay mas resultados'
+	}));
+	
+});
