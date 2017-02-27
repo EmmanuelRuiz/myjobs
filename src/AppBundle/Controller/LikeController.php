8158130c542c8411151f61b54891d90b4684cece
Juan Amaya <amayajuan95@gmail.com>
2017-02-27 18:19:27 -0500

    Sistema de likes (Incompleto)
    Falta hacer las modificaciones para los likes :(
    generar Bundles nuevos

---
+++ src/AppBundle/Controller/LikeController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Session;
+
+// clases de las entidades
+use BackendBundle\Entity\Opinion;
+use BackendBundle\Entity\User;
+use BackendBundle\Entity\Like;
+
+
+class LikeController extends Controller{
+	
+	public function likeAction($id = null){
+		
+		/*$user = $this->getUser();
+		
+		$em = $this->getDoctrine()->getManager();
+		
+		$opinion_repo = $em->getRepository('BackendBundle:Opinion');
+		// Buscamos la opinion que estamos dando like
+		$opinion = $opinion_repo->find($id);
+		
+		// Creamo un objeto de tipo Like
+		$like = new Like();
+		$like->setUser($user);
+		$like->setCompany($opinion);
+		
+		$em->persist($like);
+		$flush = $em->flush();
+		
+		if ($flush == null) {
+			$status = "Te gusta esta publicación";
+		} else {
+			$status = "No se ha podido guardar el me gusta";
+		}
+		
+		return new Response($status);*/
+	}
+	
+}
