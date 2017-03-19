a2dea8e3845b450af5c87c3ed5d9452fc7f8bbce
Emmanuel Ruiz <emmanuelre123@outlook.com>
2017-03-18 20:15:28 -0600

    verificando la ruta alv

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -22,7 +22,7 @@ class UserController extends Controller {
 
     public function loginAction(Request $request) {
         if (is_object($this->getUser())) {
-            return $this->redirect('');
+            return $this->redirect('home');
         }
 
         $authenticationUtils = $this->get('security.authentication_utils');
