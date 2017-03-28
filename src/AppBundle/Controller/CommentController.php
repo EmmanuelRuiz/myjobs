9000bfd5cabb125160894350fb35f7d155cac3c2
Juan Amaya <amayajuan95@gmail.com>
2017-03-27 23:02:56 -0500

    Imagenes
    Falta hacer que suba las imagenes

--- src/AppBundle/Controller/CommentController.php
+++ src/AppBundle/Controller/CommentController.php
@@ -13,21 +13,46 @@ use BackendBundle\Entity\Company;
 class CommentController extends Controller{
    
     public function createAction(Request $request, $id, $opinion_id){
-		$comment = new Comment();
-		$user = $this->getUser();
-	
-		
-		$commentText = trim($request->request->get("comment"));
-		
 		$em = $this->getDoctrine()->getManager();
+		$comment = new Comment();
 		
-		//$company_repo = $em->getRepository('BackendBundle:Company');
-		
-		//$company = $company_repo->find($id);
+		$user = $this->getUser();
 		
 		$opinion_repo = $em->getRepository('BackendBundle:Opinion');
 		// buscamos la opinion a la que le estamos comentando
 		$opinion = $opinion_repo->find($opinion_id);
+
+		$commentText = $request->request->get("comment");		
+	
+		// upload image
+		$image = $request->request->get("image");
+		
+		// evaluamos si hay una imagen enviandose
+		if (!empty($image) && $image != null) { // si la imagen existe
+			
+			// generamos una extensión
+			$ext = $image->guessExtension();
+			
+			// evaluamos si la imagen tiene estas extensiones
+			if ($ext == 'jpg' || $ext == 'jpeg' || $ext == 'png' || $ext == 'gif') {
+				// guardamos el nombre del archivo que viene
+				$file_name = $user->getId().time().".".$ext; 
+				// movemos a la carpeta que deseemos en el servidor
+				$file->move("uploads/comments/images", $file_name);
+				
+				// lo guaramos
+				$comment->setImage($file_name);
+				
+			} else { // si no viene con una extension correcta
+				// haces un set imagen en la entidad a null
+				$comment->setImage(null);
+			}
+		} else { // si no existe
+			// insertamos como vacio
+			$comment->setImage(null);
+		}
+		
+
 		
 		// insertamos el comentario
 		$comment->setBody($commentText);
@@ -36,9 +61,11 @@ class CommentController extends Controller{
         $comment->setUser($user);
 		$comment->setCreatedAt(new \DateTime("now"));
         $em->persist($comment);
-		
 		$em->flush(); //ejecturamos
+		
+		
         $this->addFlash('msg', 'Tu comentario se ha realizado con exito');
+		
         return $this->redirectToRoute('company_profile', array('id' => $id ));
 		
 		
