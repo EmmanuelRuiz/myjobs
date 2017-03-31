11d932e2cc614737559e7f640bf78c68a9058f67
Emmanuel Ruiz <emmanuelre123@outlook.com>
2017-03-30 19:46:08 -0600

    Muestra empresas no validadas
    Ahora muestra las empresas que en el campo status tienen un 'no',
    cambiaré ese no por invalid,
    apoko no karnal

--- src/AppBundle/Controller/AdministratorController.php
+++ src/AppBundle/Controller/AdministratorController.php
@@ -18,6 +18,20 @@ class AdministratorController extends Controller
     
     public function companiesAdminAction(Request $request)
     {
-        return $this->render('AppBundle:Administrator:administrator_companies.html.twig');
+                $em = $this->getDoctrine()->getManager();
+
+        // Hacemos una consulta a la entidad Company para que nos saque los objetos de tipo Company
+        $dql = "SELECT u FROM BackendBundle:Company u WHERE u.status = 'no'";
+        $query = $em->createQuery($dql);
+
+        $paginator = $this->get('knp_paginator');
+        $pagination = $paginator->paginate(
+			$query, $request->query->getInt('page', 1), 5
+        );
+
+        return $this->render('AppBundle:Administrator:administrator_companies.html.twig', array(
+			'pagination' => $pagination
+        ));
+        /*return $this->render('AppBundle:Administrator:administrator_companies.html.twig');*/
     }
 }
