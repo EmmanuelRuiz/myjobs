f875aff4374c4aa0c4b890148bc7fe5ec144f0c4
Juan Amaya <amayajuan95@gmail.com>
2017-04-25 12:53:29 -0500

    Detalles en los formularios de registro

--- src/AppBundle/Form/UserType.php
+++ src/AppBundle/Form/UserType.php
@@ -14,6 +14,7 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 
 class UserType extends AbstractType {
 
