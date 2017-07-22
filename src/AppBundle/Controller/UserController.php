eaa4a9a8dfa5d6a783366a6cd16432d661617d88
Juan Amaya <amayajuan95@gmail.com>
2017-07-22 13:59:36 -0500

    Login con Facebook
    (y)

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -7,7 +7,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\Session;
+
 use BackendBundle\Entity\User;
+
 use AppBundle\Form\RegisterType;
 use AppBundle\Form\UserType;
 
