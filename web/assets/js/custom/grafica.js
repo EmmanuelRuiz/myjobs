fe1862a08fa689eb3b0526c41a68c727933c3b1c
Emmanuel Ruiz <emmanuelre123@outlook.com>
2017-08-14 21:46:00 -0500

    aparecela grafica, pero sin valores
    Lo deje mal ahora, no me queda mas tiempo para probar, pero vamos bien

--- web/assets/js/custom/grafica.js
+++ web/assets/js/custom/grafica.js
@@ -1,15 +1,83 @@
-$(document).ready(function () {
-     $(".graficar").click(function () {
+$(document).ready(function() {
+    $(".graficar").click(function() {
         //var email = "hola";
 
         $.ajax({
             url: URL + '/graficar',
             //data: {email: email},
             type: 'POST',
-            success: function (response) {
-                $(".rellenar").text(response.age);
-                console.log(response.age);
+            success: function(datos) {
+                $(".rellenar").text(datos);
+                console.log(datos);
+                graficar(datos);
+
             }
         });
     });
+
+
 });
+
+var datos = [144, 89, 55, 34, 21, 13, 8, 5, 3, 2, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144];
+
+function graficar() {
+    alert("gola");
+    var w = 500;
+    var h = 300;
+
+    var svg = d3.select('.rellenar')
+        .append('svg')
+        .attr("width", w)
+        .attr("height", h);
+
+    svg.selectAll("rect")
+        .data(datos)
+        .enter()
+        .append("rect")
+        .attr("x", 0)
+        .attr("y", 0)
+        .attr("width", 20)
+        .attr("height", 100)
+
+    .attr("x", function(d, i) {
+        return i * 21 + 30;
+    })
+
+    .attr("height", function(d) {
+        return d + 50;
+    })
+
+    .attr("y", function(d) {
+            return h - d - 50;
+        })
+        .attr("fill", "SteelBlue")
+
+    //el metodo on alimenta la lista de elementos que han alimentado al svg
+    //queremos escuchar en el on el mouse over
+    //para trabajar con elementos de la grafica lo debemos seleccionar
+
+    .on("mouseover", function() {
+        d3.select(this)
+            .attr("fill", "tomato");
+    })
+
+    .on("mouseout", function(d, i) {
+        d3.select(this)
+            .attr("fill", "SteelBlue");
+    });
+
+
+    svg.selectAll("text")
+        .data(datos)
+        .enter()
+        .append("text")
+        .text(function(d) {
+            return d;
+        })
+        .attr("x", function(d, i) {
+            return i * 21 + 40;
+        })
+        .attr("y", function(d) {
+            return h - d - 53;
+        })
+}
