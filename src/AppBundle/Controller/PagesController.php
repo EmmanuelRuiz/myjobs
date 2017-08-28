bda85a08bcd696a279361d57d7d49842997762fd
Juan Amaya <amayajuan95@gmail.com>
2017-08-27 22:20:17 -0500

    Contacto

---
+++ src/AppBundle/Controller/PagesController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class PagesController extends Controller {
+
+	public function viewContactAction(Request $request) {
+		return $this->render('AppBundle:Pages:contact.html.twig');
+	}
+
+	public function contactAction(Request $request) {
+		$name = $request->get('name');
+		$email = $request->get('email');
+		$subject = $request->get('subject');
+		$message_send = $request->get('message');
+
+		$message = \Swift_Message::newInstance()
+				->setSubject('Mensaje enviado desde MyJobsAdvisor')
+				->setFrom('contacto@crealab.com.mx')
+				->setTo('contacto@crealab.com.mx')
+				->setBody(
+				$this->renderView(
+						// app/Resources/views/Emails/registration.html.twig
+						'Emails/contact.html.twig', array(
+							'message' => $message_send, 
+							'name' => $name, 
+							'email' => $email,
+							'subject' => $subject
+							)
+				), 'text/html'
+		);
+
+		$this->get('mailer')->send($message);
+
+		return $this->render('AppBundle:Pages:contact.html.twig');
+	}
+
+}
